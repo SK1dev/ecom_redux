61c3a716114120457541523d8e66995e15b89119
SK374 <sk374@protonmail.com>
2022-12-13 00:10:09 +0100

    Add firebase functionality for db, create categories

--- src/index.js
+++ src/index.js
@@ -3,8 +3,9 @@ import ReactDOM from 'react-dom/client';
 import App from './App';
 import './main.scss';
 import { BrowserRouter } from "react-router-dom";
+
 import { UserProvider } from './contexts/user.context';
-import { ProductsProvider } from './contexts/products.context';
+import { CategoriesProvider } from './contexts/categories.context';
 import { CartProvider } from './contexts/cart.context';
 
 import './index.scss';
@@ -15,11 +16,11 @@ root.render(
   <React.StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <ProductsProvider>
+        <CategoriesProvider>
           <CartProvider>
             <App />
           </CartProvider>
-        </ProductsProvider>
+        </CategoriesProvider>
       </UserProvider>
     </BrowserRouter>
   </React.StrictMode>
--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -5,28 +5,38 @@ const Home = () => {
   const categories = [
     {
       id: 1,
-      title: 'Face Oils',
+      title: 'Face',
       imageUrl: 'https://tinyurl.com/4b55j3f2'
     },
     {
       id: 2,
-      title: 'Body Oils',
+      title: 'Body',
       imageUrl: 'https://tinyurl.com/2mjf6h8j'
     },
     {
       id: 3,
-      title: 'Massage Oils',
-      imageUrl: 'https://tinyurl.com/ywzrcyff'
+      title: 'Cleanse',
+      imageUrl: 'https://tinyurl.com/2mjf6h8j'
     },
     {
       id: 4,
-      title: 'Hair Oils',
+      title: 'Hair',
       imageUrl: 'https://tinyurl.com/vznp2rs4'
     },
     {
       id: 5,
-      title: 'Butters',
-      imageUrl: 'https://tinyurl.com/4cwdtze8'
+      title: 'Hands',
+      imageUrl: 'https://tinyurl.com/4b55j3f2'
+    },
+    {
+      id: 6,
+      title: 'Massage',
+      imageUrl: 'https://tinyurl.com/ywzrcyff'
+    },
+    {
+      id: 7,
+      title: 'Scents',
+      imageUrl: 'https://tinyurl.com/vznp2rs4'
     }
   ]
   return (
