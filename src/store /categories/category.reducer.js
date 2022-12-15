f861c983afd60aec9af31905326dffc08275c33a
SK374 <sk374@protonmail.com>
2022-12-15 02:04:15 +0100

    Migrated from thunks to sagas

--- src/store /categories/category.reducer.js
+++ src/store /categories/category.reducer.js
@@ -1,27 +1,23 @@
-import { CATEGORIES_ACTION_TYPES } from './category.types';
+import { USER_ACTION_TYPES } from './user.types';
 
-export const CATEGORIES_INITIAL_STATE = {
-  categories: [],
+const INITIAL_STATE = {
+  currentUser: null,
   isLoading: false,
   error: null,
 };
 
-export const categoriesReducer = (
-  state = CATEGORIES_INITIAL_STATE,
-  action = {}
-) => {
+export const userReducer = (state = INITIAL_STATE, action) => {
   const { type, payload } = action;
 
   switch (type) {
-    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_START:
-      return {
-        ...state,
-        isLoading: true,
-      };
-    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_SUCCESS:
-      return { ...state, isLoading: false, categories: payload };
-    case CATEGORIES_ACTION_TYPES.FETCH_CATEGORIES_FAILED:
-      return { ...state, isLoading: false, error: payload };
+    case USER_ACTION_TYPES.SIGN_IN_SUCCESS:
+      return { ...state, currentUser: payload };
+    case USER_ACTION_TYPES.SIGN_OUT_SUCCESS:
+      return { ...state, currentUser: null };
+    case USER_ACTION_TYPES.SIGN_OUT_FAILED:
+    case USER_ACTION_TYPES.SIGN_IN_FAILED:
+    case USER_ACTION_TYPES.SIGN_UP_FAILED:
+      return { ...state, error: payload };
     default:
       return state;
   }
